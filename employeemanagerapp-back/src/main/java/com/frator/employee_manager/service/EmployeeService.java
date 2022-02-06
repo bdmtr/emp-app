843c196d2329d45a560ed72a0ec0b6198c061007
bdmtr <bidenko.dmtr@gmail.com>
2022-02-06 17:12:58 +0200

    adding front and back

---
+++ employeemanagerapp-back/src/main/java/com/frator/employee_manager/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.frator.employee_manager.service;
+
+import com.frator.employee_manager.exception.UserNotFoundException;
+import com.frator.employee_manager.model.Employee;
+import com.frator.employee_manager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User " + id +  " was not found"));
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+}
